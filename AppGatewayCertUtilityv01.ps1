f080b9dfaba25637dc6e865fb79946d440325f86
dus.dee <dustysusag@gmail.com>
2022-12-27 11:43:17 -0800

    Init
    Adding ps1 script and README.md

---
+++ AppGatewayCertUtilityv01.ps1
@@ -0,0 +1,333 @@
+param(
+    $ApplicationGateway,
+    $Operation,
+    [switch]$Remove
+)
+
+# Programmatic Operation selection via terminal
+switch($Operation) {
+    "Unused" {
+        $Operation = "1"
+    }
+    "KeyVault" {
+        $Operation = "2"
+    }
+    "Expiration" {
+        $Operation = "3"
+    }
+    Default {
+        $Operation = "0"
+    }
+}
+
+# Return AppGW Object after removal of certs or return interactive output
+if($Remove) {
+    $RemoveOption = "Y"
+} else {
+    $InformationPreference = "continue"
+    $RemoveOption = "N"
+}
+
+# Verify supplied object is of type PSApplicationGateway
+if(!$ApplicationGateway -or $ApplicationGateway.GetType().Name -ne "PSApplicationGateway") {
+    Write-Error "Please supply an Application Gateway PS Object"
+    Exit
+}
+
+# Get SKU type
+$sku = if($appgw.Sku.Tier -match "^Standard_v2$|^WAF_v2$") {
+    "V2"
+}elseif($appgw.Sku.Tier -match "^Standard$|^WAF$") {
+    "V1"
+}
+    else {
+    Write-Error "Error in retreiving Application Gateway SKU.  Ensure your object has an appropriate SKU tier."
+    Exit
+}
+Write-Information "`nApplication Gateway is of type: ${sku}`n"
+
+# Get Certificates
+$NonKvSslCertificates = $ApplicationGateway.SslCertificates | Where-Object { !$_.KeyVaultSecretId }
+$KvSslCertificates = $ApplicationGateway.SslCertificates | Where-Object { $_.KeyVaultSecretId }
+$Listeners = $ApplicationGateway.HttpListeners
+$ListenerCertificates = $Listeners.SslCertificate
+$AuthenticationCertificates = $ApplicationGateway.AuthenticationCertificates
+$TrustedRootCertificates = $ApplicationGateway.TrustedRootCertificates
+$TrustedClientCertificates = $ApplicationGateway.TrustedClientCertificates
+
+# Get MI, if any
+$ApplicationGatewayManagedIdentity = $ApplicationGateway.Identity
+
+# Get User Choice of Task to Perform
+While ($Operation -notmatch "1|2|3" ) {
+    Write-Information "Choose from the following options:`n[1] Check for unused certificates`n[2] Check for inaccessible Key Vault references`n[3] Check certificate expiration"
+    $Operation = Read-Host -Prompt ">"
+}
+
+# Array Diff Function, probably reinventing the wheel
+function GetUnusedCertificates($AllCerts, $AssignedCerts) {
+    $UnusedCerts = @()
+    $AllCerts | ForEach-Object {
+        if($_.Id -notin $AssignedCerts.Id) {
+            $UnusedCerts += $_
+        }
+    }
+    return $UnusedCerts
+}
+
+Switch ($Operation) {
+    "1" {
+        # SSL (Listener) Certs
+        $AllSslCertificates = @($NonKvSslCertificates)
+        $AllSslCertificates += $KvSslCertificates
+        $UnusedSslCerts = GetUnusedCertificates $AllSslCertificates $ListenerCertificates
+        Write-Information "=======================================`n`tUnused SSL Certificates`t`n======================================="
+        if($UnusedSslCerts.count -gt 0) {
+            $UnusedSslCerts | ForEach-Object {
+                Write-Information "$($_.Id.split('/')[10])`t"
+            }
+        } else {
+            Write-Information "`nNo unused SSL Certificates found"
+        }
+        if($sku -eq "V1") {
+            # Auth Certs
+            $BackendSettingsCertificates = $ApplicationGateway.BackendHttpSettingsCollection.AuthenticationCertificates
+            $UnusedBackendCerts = GetUnusedCertificates $AuthenticationCertificates $AssignedAuthenticationCertificates
+            if($UnusedBackendCerts.count -gt 0) {
+                Write-Information "=======================================`n`tUnused Auth Certificates`t`n======================================="
+                $UnusedBackendCerts | ForEach-Object {
+                    Write-Information "$($_.Id.split('/')[10])`t(Unused Auth Certificate)"
+                }
+            }
+        } else {
+            # Trusted Root Certs
+            $BackendSettingsCertificates = $ApplicationGateway.BackendHttpSettingsCollection.TrustedRootCertificates
+            $UnusedBackendCerts = getUnusedCertificates $TrustedRootCertificates $BackendSettingsCertificates
+            if($UnusedBackendCerts.count -gt 0) {
+                Write-Information "=======================================`n    Unused Trusted Root Certificates`t`n======================================="
+                $UnusedBackendCerts | ForEach-Object {
+                    Write-Information "$($_.Id.split('/')[10])`t"
+                }
+            }
+            # Trusted Client Certs
+            $SSLProfileTrustedClientCerts = $ApplicationGateway.SslProfiles.TrustedClientCertificates
+            $UnusedClientCerts = getUnusedCertificates $TrustedClientCertificates $SSLProfileTrustedClientCerts
+            if($UnusedClientCerts.count -gt 0) {
+                Write-Information "=======================================`n  Unused Trusted Client Certificates`t`n======================================="
+                $UnusedClientCerts | ForEach-Object {
+                    Write-Information "$($_.Id.split('/')[10])`t"
+                }
+            }
+        }
+        
+        $UnusedCerts = @()
+        $UnusedCerts += if($UnusedSslCerts) { $UnusedSslCerts }
+        $UnusedCerts += if($UnusedBackendCerts) { $UnusedBackendCerts }
+        $UnusedCerts += if($UnusedClientCerts) { $UnusedClientCerts }
+        # Ask User to Remove Certificates
+        if($UnusedCerts.count -gt 0) {
+            While ($RemoveOption -notmatch "Y|N" ) {
+                Write-Information "Removed Unused Certificates? (Y/N)"
+                $RemoveOption = Read-Host -Prompt ">"
+            } 
+            if($RemoveOption -eq "Y") {
+                $UnusedCerts | ForEach-Object {
+                    $name = $_.Name
+                    Switch($_.Type.split('/')[2]) {
+                        "sslCertificates" {
+                            $ApplicationGateway = Remove-AzApplicationGatewaySslCertificate -ApplicationGateway $ApplicationGateway -Name $name
+                        }
+                        "authenticationCertificates" {
+                            $ApplicationGateway = Remove-AzApplicationGatewayAuthenticationCertificate -ApplicationGateway $ApplicationGateway -Name $name
+                        }
+                        "trustedRootCertificates" {
+                            $ApplicationGateway = Remove-AzApplicationGatewayTrustedRootCertificate -ApplicationGateway $ApplicationGateway -Name $name
+                        }
+                        "trustedClientCertificates" {
+                            $ApplicationGateway = Remove-AzApplicationGatewayTrustedClientCertificate -ApplicationGateway $ApplicationGateway -Name $name
+                        }
+                    }
+                }
+            }
+        } else {
+            Write-Information "No unused certificates found"
+        }
+    }
+    "2" {
+        # Bad KV references can't be removed if attached to listener
+        $BadKvCertificateReference = @()
+        $BadKvCertificateReferenceNonRemovable = @()
+        if($KvSslCertificates.Count -gt 0) {
+            $KvSslCertificates | ForEach-Object {
+
+                # Check that user can access KV and Cert, and then check that MI has permissions to check as well.
+                $KeyVaultName = $_.KeyVaultSecretId -replace '(https*\:\/\/)(.*)(\.vault.*$)','$2'
+                $KeyVaultSecret = $_.KeyVaultSecretId -replace '(.*\/secrets\/)(\w*)(\/*\w*)','$2'
+                $Kv = Get-AzKeyVault -VaultName $KeyVaultName -ErrorAction 'silentlycontinue'
+                $KvCert = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $KeyVaultSecret -ErrorAction 'silentlycontinue'
+                if($ApplicationGatewayManagedIdentity) {
+                    $PrincipalId = $ApplicationGatewayManagedIdentity.UserAssignedIdentities.Values.PrincipalId
+                }
+
+                # Check MI Permissions to Secrets and Network Perms
+                if($Kv -and $PrincipalId) {
+                    $AccessPolicy = $Kv.AccessPolicies | Where-Object {
+                        $_.ObjectId -eq $PrincipalId
+                    }
+                    if($AccessPolicy -and ("get" -in $AccessPolicy.PermissionstoSecrets) -and $Kv.NetworkAcls.Bypass -eq "AzureServices") {
+                        $MIPerms = $true
+                    }
+                } else {
+                    $MIPerms = $false
+                }
+                
+                if(!$Kv -or !$KvCert -or !$KvCert.Enabled -or !$MIPerms) {
+                    # Check if Assigned to Listener
+                    $IsAssignedToListener = ($_.Id -in $Listeners.SslCertificate.Id)
+                    if($IsAssignedToListener) {
+                        $BadKvCertificateReferenceNonRemovable += $_
+                    } else {
+                        $BadKvCertificateReference += $_
+                    }
+                }
+                
+            }
+        } else {
+            Write-Information "No listener certificates are referencing Key Vault"
+        }
+        Write-Information "======================================`n Bad Key Vault Certificate References`t`n======================================"
+        if($BadKvCertificateReference.count -gt 0) {
+            $BadKvCertificateReference | ForEach-Object {
+                Write-Information "$($_.Id.split('/')[10])`t(Unassigned, removable)"
+            }
+        }
+        if($BadKvCertificateReferenceNonRemovable.count -gt 0) {            
+            $BadKvCertificateReferenceNonRemovable | ForEach-Object {
+                Write-Information "$($_.Id.split('/')[10])`t(Assigned to listener, non-removable)"
+            }
+            Write-Information ""
+            Write-Information "For bad Key Vault references, see the README on how to resolve these as there are limitations to what this script checks."
+        } else {
+            Write-Information "`nNo bad Key Vault certificate references found."
+        }
+
+        if($BadKvCertificateReference.length -gt 0) {
+            While ($RemoveOption -notmatch "Y|N" ) {
+                Write-Information "Removed Bad References? (Y/N)"
+                $RemoveOption = Read-Host -Prompt ">"
+            } 
+            if($RemoveOption -eq "Y") {
+                $BadKvCertificateReference | ForEach-Object {
+                    $ApplicationGateway = Remove-AzApplicationGatewaySslCertificate -ApplicationGateway $ApplicationGateway -Name $_.Name
+                }
+            }
+        }
+    }
+    "3" {
+        # Get SSL Leaf Cert Expiration as long as PFX bundled properly (Non-KV)
+        if($NonKvSslCertificates.count -gt 0 -or $KvSslCertificates.count -gt 0) {
+            Write-Output "======================================`n      SSL Certificate Expiration `t`n======================================"
+            $CertsHashTableArray = @()
+            if($NonKvSslCertificates.count -gt 0) {
+                $NonKvSslCertificates | ForEach-Object {
+                    $certData = $_.PublicCertData
+                    $certChain = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+                    $certChain.Import([System.Convert]::FromBase64String($certData),$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+    
+                    $Now = Get-Date
+                    $Expiration = $certChain[-1].NotAfter
+                    $TimeToExpiration = ($Expiration - $Now).Days
+                    $IsExpired = ($Expiration -lt $Now)
+                    if($IsExpired) {
+                        $CertsHashTableArray += @{ Name = $_.Name; Status = "Expired"; Expiration = $Expiration }
+                    } elseif($TimeToExpiration -gt 0 -and $TimeToExpiration -lt 30) {
+                        $CertsHashTableArray += @{ Name = $_.Name; Status = "Expiring Soon (<30 Days)"; Expiration = $Expiration }
+                    } else {
+                        $CertsHashTableArray += @{ Name = $_.Name; Status = "Active"; Expiration = $Expiration }
+                    }
+                }
+            }
+            # Get SSL Leaf Cert Expiration for KV Certificates
+            if($KvSslCertificates.count -gt 0) {
+                $KvSslCertificates | ForEach-Object {
+                    $KeyVaultName = $_.KeyVaultSecretId -replace '(https*\:\/\/)(.*)(\.vault.*$)','$2'
+                    $KeyVaultSecret = $_.KeyVaultSecretId -replace '(.*\/secrets\/)(\w*)(\/*\w*)','$2'
+                    $Kv = Get-AzKeyVault -VaultName $KeyVaultName -ErrorAction 'silentlycontinue'
+                    $KvCert = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $KeyVaultSecret -ErrorAction 'silentlycontinue'
+                    
+                    if($Kv) {
+                        $Now = Get-Date
+                        $Expiration = $KvCert.Expires
+                        $TimeToExpiration = ($Expiration - $Now).Days
+                        $IsExpired = ($Expiration -lt $Now)
+                        if($IsExpired) {
+                            $CertsHashTableArray += @{ Name = $_.Name; Status = "Expired"; Expiration = $Expiration }
+                        } elseif($TimeToExpiration -gt 0 -and $TimeToExpiration -lt 30) {
+                            $CertsHashTableArray += @{ Name = $_.Name; Status = "Expiring Soon (<30 Days)"; Expiration = $Expiration }
+                        } else {
+                            $CertsHashTableArray += @{ Name = $_.Name; Status = "Active"; Expiration = $Expiration }
+                        }
+                    } else {
+                        Write-Output "$($_.Name) (KV)`t(Couldn't access Key Vault, check that certificate permissions, that it exists, and is enabled)"
+                    }
+                }
+            }
+            Write-Output ($CertsHashTableArray | ForEach-Object {[PSCustomObject]$_} | Format-Table Name, Status, Expiration -AutoSize)
+        }
+        
+        # Get Trusted Root Cert Expiration
+        if($TrustedRootCertificates.count -gt 0 -or $AuthenticationCertificates.count -gt 0) {
+            Write-Output "======================================`n    Backend Certificate Expiration `t`n======================================"
+        }
+        if($TrustedRootCertificates.count -gt 0) {
+            $CertsHashTableArray = @()
+            $TrustedRootCertificates | ForEach-Object {
+                $certData = $_.Data
+                $certChain = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+                $certChain.Import([System.Convert]::FromBase64String($certData),$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+
+                $Now = Get-Date
+                $Expiration = $certChain[-1].NotAfter
+                $TimeToExpiration = ($Expiration - $Now).Days
+                $IsExpired = ($Expiration -lt $Now)
+                if($IsExpired) {
+                    $CertsHashTableArray += @{ Name = $_.Name; Status = "Expired"; Expiration = $Expiration }
+                } elseif($TimeToExpiration -gt 0 -and $TimeToExpiration -lt 30) {
+                    $CertsHashTableArray += @{ Name = $_.Name; Status = "Expiring Soon (<30 Days)"; Expiration = $Expiration }
+                } else {
+                    $CertsHashTableArray += @{ Name = $_.Name; Status = "Active"; Expiration = $Expiration }
+                }
+            }
+            Write-Output ($CertsHashTableArray | ForEach-Object {[PSCustomObject]$_} | Format-Table Name, Status, Expiration -AutoSize)
+        }
+
+        # Get Auth Cert Expiration
+        if($AuthenticationCertificates.count -gt 0) {
+            $CertsHashTableArray = @()
+            $AuthenticationCertificates | ForEach-Object {
+                $certData = $_.Data
+                $certChain = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+                $certChain.Import([System.Convert]::FromBase64String($certData),$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+
+                $Now = Get-Date
+                $Expiration = $certChain[-1].NotAfter
+                $TimeToExpiration = ($Expiration - $Now).Days
+                $IsExpired = ($Expiration -lt $Now)
+                if($IsExpired) {
+                    $CertsHashTableArray += @{ Name = $_.Name; Status = "Expired"; Expiration = $Expiration }
+                } elseif($TimeToExpiration -gt 0 -and $TimeToExpiration -lt 30) {
+                    $CertsHashTableArray += @{ Name = $_.Name; Status = "Expiring Soon (<30 Days)"; Expiration = $Expiration }
+                } else {
+                    $CertsHashTableArray += @{ Name = $_.Name; Status = "Active"; Expiration = $Expiration }
+                }
+            }
+            Write-Output ($CertsHashTableArray | ForEach-Object {[PSCustomObject]$_} | Format-Table Name, Status, Expiration -AutoSize)
+        }
+    } Default {
+        Write-Information "Valid input not detected, exiting..."
+        Exit
+    }
+}
+if($Operation -ne "3" -and $Remove) {
+    return $ApplicationGateway
+}
---
+++ README.md
@@ -0,0 +1,153 @@
+# Intro
+This utility is an interactive tool for managing Azure Application Gateway certificates.  Application Gateway does not have any native cert management functionality and there are some issues that can cause outages it if you don't proactively monitor things like unused certs, Key Vault cert references, and certificate expiration.  The tool can optionally return an App Gateway PowerShell object if you'd like to use this in automation rather than interactively.
+
+# On This Page <!-- omit in toc -->
+- [Intro](#intro)
+- [Interactive Usage](#interactive-usage)
+  - [Examples](#examples)
+  - [Implement Changes](#implement-changes)
+- [Programmatic Usage](#programmatic-usage)
+  - [Examples](#examples-1)
+- [Script Limitations](#script-limitations)
+- [Certificate Management](#certificate-management)
+  - [Unused Certificates](#unused-certificates)
+  - [Bad Key Vault References](#bad-key-vault-references)
+  - [Certificate Expiration](#certificate-expiration)
+
+# Interactive Usage
+By providing an App Gateway object, you get the interactive prompts:
+```powershell
+$appgw = Get-AzApplicationGateway -ResourceGroupName "my-rg" -Name "my-appgw"
+.\AppGatewayCertUtilityv01.ps1 -ApplicationGateway $appgw
+```
+```
+Application Gateway is of type: V2
+
+Choose from the following options:
+[1] Check for unused certificates
+[2] Check for inaccessible Key Vault references
+[3] Check certificate expiration
+>:
+```
+## Examples
+Identify unused certs:
+```
+>:1
+=======================================
+        Unused SSL Certificates
+=======================================
+sslpfx2022
+=======================================
+    Unused Trusted Root Certificates
+=======================================
+mycustomrootCA
+mytestrootCA
+=======================================
+  Unused Trusted Client Certificates
+=======================================
+mtlsauthCA
+mtlsclient1
+mtlsclient2v
+Removed Unused Certificates? (Y/N)
+>:
+```
+Identify bad Key Vault references:
+```
+>: 2
+======================================
+ Bad Key Vault Certificate References
+======================================
+kvcert1  (Unassigned, removable)
+kvcert2 (Assigned to listener, non-removable)
+Removed Bad References? (Y/N)
+>:
+```
+Check cert expiration (less than 30 days away will be marked as "Expiring Soon"):
+```
+>: 3
+======================================
+      SSL Certificate Expiration
+======================================
+
+Name    Status          Expiration
+----    ------          ----------
+ssl2022 Expired         10/30/2022 1:25:36 AM
+sslpfx2 Expiring Soon   1/15/2023 12:25:36 PM
+ssl2033 Active          10/30/2023 3:30:27 AM
+```
+
+## Implement Changes
+```powershell
+Set-AzApplicationGateway -ApplicationGateway $appgw
+```
+
+# Programmatic Usage
+If you want to use this in automation and not interactively, you have a few options:
+- Specify the option when running the script with the `Operation` flag; acceptable values are `Unused`, `KeyVault`, and `Expiration`.  These correspond to options `1`, `2`, `3` from the interactive prompts.
+- By default, these options will just return the same output from the interactive prompts to the console.  With the exception of the `Expiration` operation, you can specify the `Remove` switch to return an App Gateway object that you can assign to a variable to stage a `Set` operation.
+
+## Examples
+Return a list of unused certificates:
+```powershell
+.\AppGatewayCertUtilityv01.ps1 -ApplicationGateway $appgw -Operation "Unused"
+```
+```
+=======================================
+        Unused SSL Certificates
+=======================================
+sslpfx2022
+=======================================
+    Unused Trusted Root Certificates
+=======================================
+mycustomrootCA
+mytestrootCA
+=======================================
+  Unused Trusted Client Certificates
+=======================================
+mtlsauthCA
+mtlsclient1
+mtlsclient2v
+```
+Remove unused certificates and update the App Gateway:
+```powershell
+$appgw = .\AppGatewayCertUtilityv01.ps1 -ApplicationGateway $appgw -Operation "Unused" -Remove
+Set-AzApplicationGateway -ApplicationGateway $appgw
+```
+Check certificate expiration:
+```powershell
+$appgw = .\AppGatewayCertUtilityv01.ps1 -ApplicationGateway $appgw -Operation "Expiration"
+```
+```
+======================================
+      SSL Certificate Expiration
+======================================
+
+Name    Status          Expiration
+----    ------          ----------
+ssl2022 Expired         10/30/2022 1:25:36 AM
+sslpfx2 Expiring Soon   1/15/2023 12:25:36 PM
+ssl2033 Active          10/30/2023 3:30:27 AM
+```
+
+# Script Limitations
+- The Key Vault reference checker will identify bad references but will not allow you to remove them if they are assigned to a listener.  This is because HTTPS listeners *must* have a certificate assigned.  You will need to replace it.
+- The Key Vault checker requires that your user (in PowerShell) has `Get` permissions for both certificates and secrets, and that the App Gateway's Managed Identity has `Get` permissions for at least secrets.
+- The Key Vault checker does not account for everything in the KV Firewall.  It will check if the Firewall policy is public and if *Trusted Services can bypass this firewall* is checked, but not if you are using a service or private endpoint to allow your App Gateway to access it.
+
+# Certificate Management
+
+## Unused Certificates
+Each certificate type, listener certs, authentication certs (V1), and trusted root certs (V2), have a 100 certificate limit.  When you replace these with a new one, only the reference by its respective resource such as a listener or HTTP setting is removed; the certificate is still in your Application Gateway's state which can lead to bloating and failed updates when the limit is reached.  Manually identifying and removing certificates is a laborous task and should be automated.
+
+Trusted client certificates (V2) do not have a documented limit aside from a 25KB max CA certficiate size, but to reduce any bloating here it is advised to check for unused client certs as well.  These are assigned to SSL Profiles, which are either applied globally or to listeners.
+
+## Bad Key Vault References
+Application Gateway listeners can be configured to use a .pfx that is stored in Key Vault.  This can only be done with a Managed Identity, and if there is a failure in retreiving this certificate the Application Gateway can fail to start on it's next `Start` or `Set` operation.  Failures can be caused by the following:
+- Certificate no longer exists in the Key Vault.
+- Certificate exists but is disabled.
+- Application Gateway's Managed Identity does not have `Get` permissions for secrets, which are used to retreive the certificate.
+- Application Gateway is blocked by the Key Vault Firewall (*this is not checked by this script if using IP based access, or Service or Private endpoints*)
+
+If there are bad Key Vault references, and these are unused, they can be removed like any other certificate.  However, if they are assigned to a listener, it must first be replaced by another certificate since HTTPS listeners *must* have a .pfx certificate assigned.
+
+## Certificate Expiration
